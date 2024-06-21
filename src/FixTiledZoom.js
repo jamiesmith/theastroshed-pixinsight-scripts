122bd8c8081dc7c0f395429b532a7252d6c3d782
Jamie Smith <jamie@cosmo.jamieandamy.com>
2024-06-20 21:43:11 -0400

    prep for PI repo

--- src/FixTiledZoom.js
+++ src/FixTiledZoom.js
@@ -18,7 +18,7 @@
    this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#feature-id    Fix Tiled Zoomoom_v0.1 : TheAstroShed > Fix the zoom for tiled windows
+#feature-id    FixTiledZoomoom_v0.1 : TheAstroShed > Fix the zoom for tiled windows
 #feature-info  This script renames the target view after the filter name
 
 #include <pjsr/TextAlign.jsh>
